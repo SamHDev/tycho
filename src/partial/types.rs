d5b69ce9479209795943be85ec2448cd46c08d38
Sam Huddart <sam02h.huddart@gmail.com>
2021-03-16 00:25:49 +0000

    parital

--- src/partial/types.rs
+++ src/partial/types.rs
@@ -1,19 +1,54 @@
 use std::io::{Read, Cursor, Write};
 use crate::collections::Struct;
-use crate::marshall_vec;
+use crate::{marshall_vec, Value};
+use byteorder::ReadBytesExt;
 
-pub struct PartialHandle<T> {
-    buffer: Vec<u8>,
-    buffer_position: usize,
-    cursor_position: usize,
-    reader: T
+pub enum PartialReadError {
+    IoError(std::io::Error),
+    PointerFlushed { head: usize, position: usize }
 }
 
-impl<T> PartialHandle<T> {
-    pub fn new(handle: T) {
+pub struct PartialSource<R: Read> {
+    buffer: Vec<u8>,
+    head: usize,
+    read: R
+}
+
+impl PartialSource<Cursor<Vec<u8>>> {
+    pub fn with_bytes(buffer: Vec<u8>) -> Self {
+        let cursor = Cursor::new(buffer);
+        Self::with_reader(cursor)
+    }
+}
+
+impl<R: Read> PartialSource<R> {
+    pub fn with_reader(read: R) -> Self { Self { buffer: vec![], head: 0, read } }
+
+
+    pub fn read_at(&mut self, pos: usize) -> Result<PartialReadError, E> {
+        match
+    }
+
+    pub fn flush(&mut self) {
+        self.head += self.buffer.len();
+        self.buffer.clear();
+    }
+
+    pub fn head(&mut self) -> PartialPointer<R> {
 
     }
 }
+
+
+pub struct PartialPointer<R: Read> {
+    position: usize,
+    size: usize
+}
+
+pub enum PartialElement<R: Read> {
+
+}
+
 
 #[test]
 fn test() {
@@ -29,6 +64,8 @@ fn test() {
 
     println!("{:?}", bytes);
 
+    let mut reader = PartialSource::with_bytes(bytes);
 
-
+    let name = reader.head();
+    // println!("{:?}", name)
 }
